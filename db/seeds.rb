e13906dd49f71129a7e3c4d774ea4209119fbaad
kulas115 <kulas115@gmail.com>
2017-11-27 21:21:36 +0100

    Implemented custom scopes for portfolio items

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,11 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+3.times do |topic|
+  Topic.create!(
+    title: "Topic #{topic}"
+  )
+end
+
+puts "3 Topics created"
 
 10.times do |blog|
   Blog.create!(
     title: "My Blog Post #{blog}",
-    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse condimentum nunc purus, in vehicula erat venenatis non. Nulla facilisi. Vivamus porta turpis non ultricies faucibus. Sed ac tellus ut ex iaculis pulvinar ut ac sem. Proin ornare mauris eu purus hendrerit, luctus commodo ex blandit. Proin at ultrices metus. In et massa arcu. Morbi cursus lobortis neque a interdum. Proin lectus sem, facilisis sit amet tincidunt sit amet, vulputate id arcu. Quisque id ullamcorper ligula. In et tellus vel sapien dignissim pulvinar ut id lorem. Donec dignissim vel eros in laoreet. Praesent in imperdiet turpis. Nam ut tortor vitae odio varius egestas. Donec non nisi est."
+    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse condimentum nunc purus, in vehicula erat venenatis non. Nulla facilisi. Vivamus porta turpis non ultricies faucibus. Sed ac tellus ut ex iaculis pulvinar ut ac sem. Proin ornare mauris eu purus hendrerit, luctus commodo ex blandit. Proin at ultrices metus. In et massa arcu. Morbi cursus lobortis neque a interdum. Proin lectus sem, facilisis sit amet tincidunt sit amet, vulputate id arcu. Quisque id ullamcorper ligula. In et tellus vel sapien dignissim pulvinar ut id lorem. Donec dignissim vel eros in laoreet. Praesent in imperdiet turpis. Nam ut tortor vitae odio varius egestas. Donec non nisi est.",
+    topic_id: Topic.last.id
   )
 end
 
@@ -24,14 +32,26 @@ end
 
 puts "5 skilss created"
 
-9.times do |portfolio_item|
+8.times do |portfolio_item|
   Portfolio.create!(
     title: "Portfolio title: #{portfolio_item}",
-    subtitle: "My greate service",
+    subtitle: "Ruby on Rails",
     body: "Interdum et malesuada fames ac ante ipsum primis in faucibus. Nullam hendrerit justo massa, ut sodales sapien laoreet nec. Etiam feugiat dui viverra metus placerat, at dignissim dui mattis. Sed imperdiet nibh eros, sed elementum tortor consequat ac. Cras vestibulum luctus laoreet. Phasellus fermentum, sem eget bibendum consequat, justo neque tincidunt lorem, non ullamcorper magna velit porttitor tortor. Nulla felis elit, tincidunt eget commodo in, porta quis leo. Etiam at ipsum ac nisl accumsan vehicula nec ultrices ipsum" ,
     main_image: "http://via.placeholder.com/600x400",
     thumb_image: "http://via.placeholder.com/350x200"
   )
 end
 
-puts "9 portfolio items created"
+puts "8 portfolio items created"
+
+1.times do |portfolio_item|
+  Portfolio.create!(
+    title: "Portfolio title: #{portfolio_item}",
+    subtitle: "Angular",
+    body: "Interdum et malesuada fames ac ante ipsum primis in faucibus. Nullam hendrerit justo massa, ut sodales sapien laoreet nec. Etiam feugiat dui viverra metus placerat, at dignissim dui mattis. Sed imperdiet nibh eros, sed elementum tortor consequat ac. Cras vestibulum luctus laoreet. Phasellus fermentum, sem eget bibendum consequat, justo neque tincidunt lorem, non ullamcorper magna velit porttitor tortor. Nulla felis elit, tincidunt eget commodo in, porta quis leo. Etiam at ipsum ac nisl accumsan vehicula nec ultrices ipsum" ,
+    main_image: "http://via.placeholder.com/600x400",
+    thumb_image: "http://via.placeholder.com/350x200"
+  )
+end
+
+puts "1 portfolio item created"
