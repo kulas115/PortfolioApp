23041b346fe724d0e49e9dee0ac49e414dab07b1
kulas115 <kulas115@gmail.com>
2017-12-08 15:03:08 +0100

    Experimenting with debugging

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,12 +8,18 @@ class User < ApplicationRecord
 
   def first_name
     self.name.split.first
-
   end
 
   def last_name
     self.name.split.last
-
   end
-
 end
+
+  def get_facebook_messages
+    begin
+      contacts_fb
+      @messages = retrieves_messages
+    rescue IOError => e
+      flash[:error] = "Error occured contacting facebook: #{e}"
+    end
+  end
