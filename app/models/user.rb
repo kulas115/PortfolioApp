407e7202883a540a73082cc5092782020a927f00
kulas115 <kulas115@gmail.com>
2017-12-10 09:19:52 +0100

    Added installation requirements for petergate

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,13 @@
 class User < ApplicationRecord
+  ############################################################################################
+  ## PeterGate Roles                                                                        ##
+  ## The :user role is added by default and shouldn't be included in this list.             ##
+  ## The :root_admin can access any page regardless of access settings. Use with caution!   ##
+  ## The multiple option can be set to true if you need users to have multiple roles.       ##
+  petergate(roles: [:admin, :editor], multiple: false)                                      ##
+  ############################################################################################
+
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -15,11 +24,11 @@ class User < ApplicationRecord
   end
 end
 
-  def get_facebook_messages
-    begin
-      contacts_fb
-      @messages = retrieves_messages
-    rescue IOError => e
-      flash[:error] = "Error occured contacting facebook: #{e}"
-    end
-  end
+  #def get_facebook_messages
+  #  begin
+  #    contacts_fb
+  #    @messages = retrieves_messages
+  #  rescue IOError => e
+  #    flash[:error] = "Error occured contacting facebook: #{e}"
+  #  end
+  #end
