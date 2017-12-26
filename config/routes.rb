abb4bf1cb28149809e13ccdaf85b7d99039b14d1
Kulas115 <kulas115@gmail.com>
2017-12-26 09:56:16 +0100

    Created a comment broadcast, configured the websocket route and updated the comment model with a callback

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :comments
+  #resources :comments -> not needed with ActionCable
   devise_for :users, path: '', path_names: {sign_in: 'login', sign_out: 'logout', sign_up: 'register'}
   resources :portfolios, except: [:show] do
     put :sort, on: :collection
@@ -7,7 +7,6 @@ Rails.application.routes.draw do
   get 'angular-items', to: 'portfolios#angular'
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
 
-  root to: 'pages#home'
   get 'about',   to: 'pages#about'
   get 'contact', to: 'pages#contact'
   get 'tech-news', to: 'pages#tech_news'
@@ -18,4 +17,7 @@ Rails.application.routes.draw do
     end
   end
 
+  mount ActionCable.server => '/cable'
+
+  root to: 'pages#home'
 end
