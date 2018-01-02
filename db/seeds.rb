654361cef0130b0827a9059f57750d2eba8a0119
Kulas115 <kulas115@gmail.com>
2018-01-02 19:31:59 +0100

    Updated seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create!(
+  email:                 "test@test.com",
+  password:              "foobar",
+  password_confirmation: "foobar",
+  name:                  "Admin User",
+  roles:                 "site_admin"
+)
+
+User.create!(
+  email:                 "test2@test.com",
+  password:              "foobar",
+  password_confirmation: "foobar",
+  name:                  "Regular User"
+)
+
+puts "1 Regular user created"
+
 3.times do |topic|
   Topic.create!(
     title: "Topic #{topic}"
