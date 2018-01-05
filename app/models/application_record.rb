03f2029d9752f8be6ff576213bcd1d143c4a70b9
Kulas115 <kulas115@gmail.com>
2018-01-05 10:04:33 +0100

    Removed redundant code

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,18 +1,3 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
 end
-
-require 'rubygems'
-require 'httparty'
-
-class God
-  include HTTParty
-  base_uri "pairguru-api.herokuapp.com/api/v1/movies/"
-
-  def content
-    self.class.get('Godfather')
-  end
-end
-
-god = God.new
-puts god.content
