73a5b33f1591b034940c450276440e142efcfd71
Kulas115 <kulas115@gmail.com>
2018-02-20 14:53:17 +0100

    Added test for file uploads along with example png file

--- spec/features/portfolios_spec.rb
+++ spec/features/portfolios_spec.rb
@@ -10,12 +10,20 @@ RSpec.feature "Portfolios", type: :feature, js: true, slow: true do
     expect {
       fill_in "Title", with:   "Test Portfolio"
       fill_in "Content", with: "Some content"
+      attach_file "portfolio[main_image]", "#{Rails.root}/spec/files/test.png"
+      attach_file "portfolio[thumb_image]", "#{Rails.root}/spec/files/test.png"
       click_button "Save Portfolio"
     }.to change(Portfolio, :count).by(1)
+
+    # byebug
 
     aggregate_failures do
       expect(page).to have_content "Your portfolio item is now live."
       expect(page).to have_content "Test Portfolio"
+      expect(page).to have_css ("img[src*='test.png']")
     end
+
+    click_link "Test Portfolio"
+    expect(page).to have_css ("img[src*='test.png']")
   end
 end
