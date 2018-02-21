c877e841008474d36a06774dcf6be365391007cb
Kulas115 <kulas115@gmail.com>
2018-02-21 11:15:10 +0100

    Fixed and simplified comment spec using shoulda matchers

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,25 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  # let(:comment) { FactoryBot.create(:comment) }
-
-  before do
-    comment = FactoryBot.create(:comment)
-  end
+  let(:comment) { FactoryBot.create(:comment) }
 
   it { is_expected.to validate_presence_of :content }
+  it { is_expected.to validate_length_of(:content).is_at_least(5) }
+  it { is_expected.to validate_length_of(:content).is_at_most(1000) }
 
-  xit "is valid with content 5-1000 chars long" do
-    expect(comment).to be_valid
-  end
-
-  xit "is invalid with content shorter than 5 chars" do
-    comment.content = "foo"
-    expect(comment).to_not be_valid
-  end
-
-  xit "is invalid with content longer than 1000 chars" do
-    comment.content = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-    expect(comment).to_not be_valid
-  end
 end
