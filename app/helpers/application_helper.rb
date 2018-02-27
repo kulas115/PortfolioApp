d06ee8d92d7223670c39964d3c6fac017ad26be7
Kulas115 <kulas115@gmail.com>
2018-02-27 12:53:22 +0100

    Changed nav_helper to pass class for tag

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -49,11 +49,11 @@ module ApplicationHelper
     ]
   end
 
-  def nav_helper style, tag_type
+  def nav_helper style, tag_type, tag_class=nil
     nav_links = ''
 
     nav_items.each do |item|
-      nav_links << "<#{tag_type}><a href='#{item[:url]}' class='#{style} #{active? item[:url]}'>#{item[:title]}</a></#{tag_type}>"
+      nav_links << "<#{tag_type} class='#{tag_class}'><a href='#{item[:url]}' class='#{style} #{active? item[:url]}'>#{item[:title]}</a></#{tag_type}>"
     end
 
     nav_links.html_safe
@@ -76,22 +76,23 @@ module ApplicationHelper
   end
 
   class CodeRayify < Redcarpet::Render::HTML
-    def block_code(code, language)
-      CodeRay.scan(code,language).div
-    end
-  end
+     def block_code(code, language)
+       CodeRay.scan(code,language).div
+     end
+   end
 
-  def markdown(text)
-    coderayified = CodeRayify.new(filter_html: true, hard_wrap: true)
+   def markdown(text)
+     coderayified = CodeRayify.new(filter_html: true, hard_wrap: true)
 
-    options = {
-      fenced_code_blocks: true,
-      no_intra_emphasis: true,
-      autolink: true,
-      lax_html_blocks: true,
-    }
+     options = {
+       fenced_code_blocks: true,
+       no_intra_emphasis: true,
+       autolink: true,
+       lax_html_blocks: true,
+     }
 
-    markdown_to_html = Redcarpet::Markdown.new(coderayified, options)
-    markdown_to_html.render(text).html_safe
-  end
+     markdown_to_html = Redcarpet::Markdown.new(coderayified, options)
+     markdown_to_html.render(text).html_safe
+   end
+
 end
